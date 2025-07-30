ff8463624a69e9147d20f321f38801be003af0ea
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-07-30 02:27:46 +0000

    sets up Marp

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,12 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const classes = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/classes" }),
+const lessons = defineCollection({
+  loader: glob({ pattern: "*/lesson.md", base: "./src/content/classes" }),
   schema: z.object({ title: z.string() }),
 });
+const slides = defineCollection({
+  loader: glob({ pattern: "*/slides.md", base: "./src/content/classes" }),
+});
 
-export const collections = { classes };
+export const collections = { lessons, slides };
