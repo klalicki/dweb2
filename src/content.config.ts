45678cb53e3a16c33b5cff48a227020f6ff2dcf4
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-08-03 14:56:14 +0000

    changes routing to place lesson and slides in the same folder

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,25 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const lessons = defineCollection({
-  loader: glob({ pattern: "*/lesson.md", base: "./src/content/classes" }),
+  loader: glob({
+    pattern: "*/lesson.md",
+    base: "./src/content/classes",
+    generateId: ({ entry }) => {
+      console.log(entry.split("/"));
+      return entry.split("/")[0];
+    },
+  }),
   schema: z.object({ title: z.string() }),
 });
 const slides = defineCollection({
-  loader: glob({ pattern: "*/slides.md", base: "./src/content/classes" }),
+  loader: glob({
+    pattern: "*/slides.md",
+    base: "./src/content/classes",
+    generateId: ({ entry }) => {
+      console.log(entry);
+      return entry.split("/")[0];
+    },
+  }),
 });
 
 export const collections = { lessons, slides };
